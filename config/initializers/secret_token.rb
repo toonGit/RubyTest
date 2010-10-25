93f218def45431b0813985356679e5c43dacd451
thanrutai <Toon@.(none)>
2010-10-25 22:48:26 +0700

    try commit folder app and other (more than one)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestR19::Application.config.secret_token = '4822d67c1bb98640764abdb100df738574f9c446d608c74a740d0059f7541b456c060a365c0bcea11be8eef321d382054b5152411b316feeb8da53e090552c68'
